a95ef1ba92eef4ee901b3ac9ac2c129c7bb96371
Deivid Duarte <deividdua@gmail.con>
2023-04-07 18:33:52 -0300

    Added domain nip.io

--- provisiona_deploy.sh
+++ provisiona_deploy.sh
@@ -8,5 +8,14 @@ kubectl create clusterrolebinding cluster-admin-binding \
 # Instala ingress controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.7.0/deploy/static/provider/cloud/deploy.yaml
 
+# Aguarda LoadBalancer gerar ip
+sleep 80
+
+# Obtem ip do ingress loadbalancer e armazena na variavel
+export NGINX_INGRESS_IP=$(kubectl get svc -n ingress-nginx | awk '{print " " $4}' | sed 's/ //g' | sed '1d' | sed '2d')
+
+# Insere ip do Loadbalancer no hostname do ingress
+sed -i "s/teste/${NGINX_INGRESS_IP}/g" manifests/simpleapp-ingress.yaml
+
 # Aplica os manifestos no kubernetes
 kubectl apply -f ./manifests
